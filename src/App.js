82da0791a73243e8ee799fb1a4d330ef7b18678b
Ran Malki <ran.malki2@gmail.com>
2021-07-24 20:05:25 +0300

    avatar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Avatar from "./Avatar/Avatar.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Avatar src="https://i.ibb.co/FVhKPqK/bfcecbd1-6a15-4d50-a41c-aa142d63c003.jpg" />
+		</div>
+	);
 }
 
 export default App;
